3ecc9a3d7b2a9cc732eb1e2f31fff53b0ea6cd4e
kuboy275 <caubebandu96@gmail.com>
2021-03-22 17:05:36 +0700

    nghia update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
+
     $('#year').on('change', function() {
         $('.data').hide();
         $('#' + $(this).val()).fadeIn(2000);
     }).change();
+
+    $('.delete-item').on('click', function(e) {
+        e.preventDefault();
+        Swal.fire({
+            icon: 'question',
+            title: 'Bạn muốn xóa item này',
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: `Yes`,
+            denyButtonText: `No`,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire('Xóa thành công!', '', 'success')
+            } else if (result.isDenied) {
+                Swal.fire('Changes are not saved', '', 'error')
+            }
+        })
+
+    })
+
+
 });
